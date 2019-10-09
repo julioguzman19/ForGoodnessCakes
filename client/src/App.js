da9bfa157f8f7beed4b3939f329cbbf8d7891765
Isaac Fiuza <fiuzaisaac@hotmail.com>
2019-10-08 21:38:27 -0600

    client and server createt

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+// client/src/App.js
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div> <h2>For Goodness Cakes</h2>
+      <nav>HOME - MENU - ABOUT</nav></div>;
+  }
+}
+
+export default App;
