1e47f88859cc6365be956167b83cc9e796faaf23
Isaac Fiuza <fiuzaisaac@hotmail.com>
2019-10-22 18:43:53 -0600

    adding menu and checkout button

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./style.css";
+const logo = require('./napoleon.jpg');
 
 function Main() {
   return (
      <div>
 
-<div id="panel"></div>
+<div id="panel1">
+
+<img src={logo} width="510px" height="390px"  />
+
+</div>
 
 
 </div>
