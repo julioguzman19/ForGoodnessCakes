b34d0b2265d99ef79957f3a4a317356254909a52
Jay Sumampong <jaysumampong@gmail.com>
2019-10-22 21:17:52 -0600

    changed the layout of menu

--- client/src/components/Menu/index.js
+++ client/src/components/Menu/index.js
@@ -3,21 +3,24 @@ import "./style.css";
 
 function Menu() {
   return (
-     <div>
-                <h2>Your Order</h2>
-          <h4>Please Choose From Our Menu : </h4>
+
+    <div class="row" class="container">
+      <div class="col s12">
+
+      <h2>Your Order</h2>
+      <h4>Please Choose From Our Menu : </h4>
 
 
-<div id="flip"><h2>Our Pies For $10</h2></div>
-<div id="panel"><h5>Lemon Pie</h5>
-<h5>Apple Pie</h5><h5>Pumpkin </h5><h5>Napoleon</h5>
-<h5>Strawberry Madness</h5><h5>Dark Chocolate</h5>
-<h5>Orange Cake</h5><h5>Banana Cherry</h5>
-</div>
+      <div id="flip"><h2>Our Pies For $10</h2></div>
+      <div id="panel"><h5>Lemon Pie</h5>
+        <h5>Apple Pie</h5><h5>Pumpkin </h5><h5>Napoleon</h5>
+        <h5>Strawberry Madness</h5><h5>Dark Chocolate</h5>
+      </div>
 
+      </div>
 
-</div>
-);
+    </div>
+  );
 }
 
 export default Menu;
