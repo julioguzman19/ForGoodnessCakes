3a6a52a80b33cd3e60da8419b72c1b2d96bf7a38
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-10-25 22:49:20 -0600

    adjusting font sizes

--- client/src/components/Menu/index.js
+++ client/src/components/Menu/index.js
@@ -7,15 +7,15 @@ function Menu() {
     <div class="row" class="container">
       <div class="col s12">
 
-      <h2>Your Order</h2>
-      <h4>Please Choose From Our Menu : </h4>
+        <h4>Your Order</h4>
+        <h4>Please Choose From Our Menu : </h4>
 
 
-      <div id="flip"><h2>Our Pies For $10</h2></div>
-      <div id="panel"><h5>Lemon Pie</h5>
-        <h5>Apple Pie</h5><h5>Pumpkin </h5><h5>Napoleon</h5>
-        <h5>Strawberry Madness</h5><h5>Dark Chocolate</h5>
-      </div>
+        <div id="flip"><h4>Our Pies For $10</h4></div>
+        <div id="panel"><h5>Lemon Pie</h5>
+          <h5>Apple Pie</h5><h5>Pumpkin </h5><h5>Napoleon</h5>
+          <h5>Strawberry Madness</h5><h5>Dark Chocolate</h5>
+        </div>
 
       </div>
 
